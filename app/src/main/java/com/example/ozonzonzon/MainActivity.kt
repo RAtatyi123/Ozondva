f85f9450de367c12edf1d1bb392766575f81d874
RAtatyi123 <kolokolinikofstepan@mail.ru>
2022-10-13 16:43:49 +0300

    Сделал скров вьюппрр

--- app/src/main/java/com/example/ozonzonzon/MainActivity.kt
+++ app/src/main/java/com/example/ozonzonzon/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.ozonzonzon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageButton
+
+
+lateinit var buttonima : ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+    }
+
+    fun hendselector(view: View) {
+
+        var abobka = Intent(this, CategoryActivity::class.java)
+        startActivity(abobka)
+
     }
 }
